5106f380c817fdee9d30df8fb22806fe707eb488
Georgi Petrov <hailanimepenchou@gmail.com>
2023-01-03 16:44:35 +0200

    add cat validation

--- src/models/Cat.js
+++ src/models/Cat.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const catSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
+    name: {
+        type: String,
+        required: [true, 'Movie title is required'] // with message
+    },
+    age: {
+        type: Number,
+        required: true // without message,
+    },
     description: String,
     imageUrl: String,
 });
@@ -10,6 +16,15 @@ const catSchema = new mongoose.Schema({
 catSchema.methods.getInfo = function() {
     return `${this.name} - ${description}`;
 }
+
+catSchema.virtual('isNewborn')
+    .get(function() {
+        return age <= 3;
+    })
+
+catSchema.path('name').validate(function() {
+    return this.name.length > 2 && this.name.length < 25;
+}, `Name should be between 2 and 25 chars!`);
 
 const Cat = mongoose.model('Cat', catSchema);
 
